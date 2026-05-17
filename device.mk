401b3381b00e5adbc173f2b652ec3acb921dda3a
ansh-m14x <ansh981786@gmail.com>
2026-05-17 20:13:33 +0530

    Create device.mk

---
+++ device.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH := device/oppo/OP4C7D
+
+# Recovery UI Theme Configuration
+TARGET_RECOVERY_UI_SCREEN_WIDTH := 720
+TARGET_RECOVERY_UI_SCREEN_HEIGHT := 1560
+
+# Adjust margins to handle any screen notches/curved corners
+TARGET_RECOVERY_UI_MARGIN_HEIGHT := 100
+TARGET_RECOVERY_UI_MARGIN_WIDTH := 0
+
