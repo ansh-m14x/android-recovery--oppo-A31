c569a496eed4b1685ef2451e6774cd921455fad7
ansh-m14x <ansh981786@gmail.com>
2026-05-17 20:11:07 +0530

    Update and rename twrp_OP4C7D.mk to lineage_OP4C7D.mk

---
+++ lineage_OP4C7D.mk
@@ -0,0 +1,38 @@
+#
+# Copyright (C) 2019 The Android Open Source Project
+# Copyright (C) 2026 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Inherit from LineageOS common recovery configurations
+$(call inherit-product, vendor/lineage/config/common.mk)
+
+# Inherit from the local device setup
+$(call inherit-product, device/oppo/OP4C7D/device.mk)
+
+# Device identifiers. This must come after all inclusions
+PRODUCT_DEVICE := OP4C7D
+PRODUCT_NAME := lineage_OP4C7D
+PRODUCT_BRAND := oppo
+PRODUCT_MODEL := OPPO A31
+PRODUCT_MANUFACTURER := oppo
+
+# Build fingerprint/properties configuration overrides
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=OP4C7D \
+    TARGET_DEVICE=OP4C7D
